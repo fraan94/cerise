3cc915d4ee8d15de7969f8acbe6eb7ccd82c9099
fraan94 <141100160+fraan94@users.noreply.github.com>
2026-03-29 17:25:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,125 @@
 const productos = [
-
-{
-nombre:"Top Rosa",
-precio:1500,
-imagen:"images/top1.jpg",
-talles:["S","M","L"]
-},
-
-{
-nombre:"Top Beige",
-precio:1300,
-imagen:"images/top2.jpg",
-talles:["S","M","L"]
-},
-
-{
-nombre:"Top Arena",
-precio:1800,
-imagen:"images/top3.jpg",
-talles:["S","M","L"]
-},
-
-{
-nombre:"Top 4",
-precio:1600,
-imagen:"images/top4.jpg",
-talles:["S","M","L"]
-},
-
-
+    {
+        nombre: "Top Rosa",
+        precio: 1500,
+        imagen: "images/top1.jpg",
+        talles: ["S", "M", "L"]
+    },
+    {
+        nombre: "Top Beige",
+        precio: 1300,
+        imagen: "images/top2.jpg",
+        talles: ["S", "M", "L"]
+    },
+    {
+        nombre: "Top Arena",
+        precio: 1800,
+        imagen: "images/top3.jpg",
+        talles: ["S", "M", "L"]
+    },
+    {
+        nombre: "Top 4",
+        precio: 1600,
+        imagen: "images/top4.jpg",
+        talles: ["S", "M", "L"]
+    },
 ]
 
 let carrito = []
 let total = 0
-function formatearPrecio(precio){
 
-return "$" + precio.toLocaleString("es-UY")
-
+function formatearPrecio(precio) {
+    return "$" + precio.toLocaleString("es-UY")
 }
 const contenedor = document.getElementById("productos-container")
-
-productos.forEach((p,i)=>{
-
-let html = `
+productos.forEach((p, i) => {
+    let html = `
 <div class="producto">
-
 <img src="${p.imagen}">
-
 <h3>${p.nombre}</h3>
-
 <p>${formatearPrecio(p.precio)}</p>
-
 <div class="talles">
-${p.talles.map(t=>`<button onclick="agregar(${i},'${t}')">${t}</button>`).join("")}
+${p.talles.map(t => `<button onclick="agregar(${i},'${t}')">${t}</button>`).join("")}
 </div>
-
 </div>
 `
-
-contenedor.innerHTML += html
-
+    contenedor.innerHTML += html
 })
-
-function agregar(i,talle){
-
-let p = productos[i]
-
-carrito.push({
-
-nombre:p.nombre,
-precio:p.precio,
-talle:talle
-
-})
-
-total += p.precio
-
-render()
-
+function agregar(i, talle) {
+    let p = productos[i]
+    carrito.push({
+        nombre: p.nombre,
+        precio: p.precio,
+        talle: talle
+    })
+    total += p.precio
+    render()
+}
+function render() {
+    const lista = document.getElementById("cart-items")
+    lista.innerHTML = ""
+    carrito.forEach(p => {
+        const li = document.createElement("li")
+        li.innerText = `${p.nombre} (${p.talle}) - ${formatearPrecio(p.precio)}`
+        lista.appendChild(li)
+    })
+    document.getElementById("total").innerText = total.toLocaleString("es-UY")
 }
 
-function render(){
-
-const lista = document.getElementById("cart-items")
-
-lista.innerHTML = ""
-
-carrito.forEach(p=>{
-
-const li = document.createElement("li")
-
-li.innerText = `${p.nombre} (${p.talle}) - ${formatearPrecio(p.precio)}`
-
-lista.appendChild(li)
-
+function scrollProductos() {
+    document.getElementById("productos").scrollIntoView({
+        behavior: "smooth"
+    })
+}
+const tituloOriginal = document.title
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+        document.title = "Volvé, tenemos algo para ti💕"
+    } else {
+        document.title = tituloOriginal
+    }
 })
 
-document.getElementById("total").innerText = total.toLocaleString("es-UY")
-
-}
-
-function toggleCart(){
-
+const menu = document.getElementById("menu")
 const cart = document.getElementById("cart")
 const overlay = document.getElementById("overlay")
 
-cart.classList.toggle("open")
-overlay.classList.toggle("active")
-
+function abrirMenu(){
+menu.classList.add("open")
+cart.classList.remove("open")
+overlay.classList.add("active")
+document.body.style.overflow = "hidden"
 }
-function scrollProductos(){
+function abrirCarrito(){
+cart.classList.add("open")
+menu.classList.remove("open")
+overlay.classList.add("active")
+document.body.style.overflow = "hidden"
+}
+function cerrarTodo(){
+menu.classList.remove("open")
+cart.classList.remove("open")
+overlay.classList.remove("active")
+document.body.style.overflow = "auto"
+}
 
-document.getElementById("productos").scrollIntoView({
-
-behavior:"smooth"
-
+let startX = 0
+menu.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX
 })
-
-}
+menu.addEventListener("touchend", (e) => {
+    let endX = e.changedTouches[0].clientX
+    if(startX - endX > 50){ // swipe hacia la izquierda
+        cerrarTodo()
+    }
+})
+let startXcart = 0
+menu.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX
+})
+menu.addEventListener("touchend", (e) => {
+    let endX = e.changedTouches[0].clientX
+    if(startX - endX > 50){ // swipe hacia la izquierda
+        cerrarTodo()
+    }
+})
